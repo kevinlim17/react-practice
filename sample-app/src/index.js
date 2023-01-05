ab8dc8142ad610c60150906745d78fc915f413b1
Seung Hyeon Lim <kevinlsh17@khu.ac.kr>
2023-01-06 00:15:44 +0900

    Add: Learn about React Elements

--- sample-app/src/index.js
+++ sample-app/src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import App from './App';
 import Library from './Chapter03/Library';
+import Clock from './Chapter_Elements/Clock';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+/**
 root.render(
   <React.StrictMode>
     <Library />
   </React.StrictMode>
 );
+**/
+
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
