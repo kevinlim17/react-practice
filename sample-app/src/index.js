b49b9343b75c8fb79c3092cb2025dbc74a6edeaf
Seung Hyeon Lim <kevinlsh17@khu.ac.kr>
2023-01-08 23:48:59 +0900

    Add: Learn about React State and Lifecycle

--- sample-app/src/index.js
+++ sample-app/src/index.js
@@ -7,14 +7,14 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 import Library from './Chapter03/Library';
 import Clock from './Chapter_Elements/Clock';
-**/
-
 import CommentList from './Chapter_Components/CommentList';
+**/
+import NotificationList from './Chapter_State/NotificationList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CommentList />
+    <NotificationList />
   </React.StrictMode>
 );
 
